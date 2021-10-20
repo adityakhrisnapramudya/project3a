ee45e268ee95711270e210265de951aae3bc2e29
adityakhrisnapramudya <adityakhrisna1501@gmail.com>
2021-10-20 11:24:11 +0700

    Upload awal project

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+public function index()
+    {
+        echo "Selamat Datang DI Website Aditya";
+    }
+
+}
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+   <b> Halo Kawan.. yuk kita belajar web programming!!</b> <br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
